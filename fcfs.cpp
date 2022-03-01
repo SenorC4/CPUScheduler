9d9869684d6940bcdd32abe12b660ed232a53119
Luke LeCain <lukelecain@gmail.com>
2022-03-01 13:43:04 -0800

    fcfs

---
+++ fcfs.cpp
@@ -0,0 +1,13 @@
+//Author Luke LeCain
+//Scheduler project OS
+
+#include <iostream>
+
+using namespace std;
+
+public void fcfsScheduler(int[] arival, int[] burst){
+	cout << "fcfs works";
+	for(int i = 0; i < arival.size(); i++){
+		cout << "Arrival time: " << arrival[i] << " Burst: " << burst[i];
+	}
+}
