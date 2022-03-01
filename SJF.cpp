15ca3d2bb28638299195c15715bcb10ead7f2eab
LoginiStrong <83607947+LoginiStrong@users.noreply.github.com>
2022-03-01 15:05:42 -0800

    Add files via upload
    Totally fixed SJF I think

---
+++ SJF.cpp
@@ -0,0 +1,13 @@
+//This is the algorithm for the Shortest Job First
+#include <iostream>
+using namespace std;
+
+void sjfScheduler(int arrival[], int burst[], int priority[])
+{
+	cout << "This is the shortest job first scheduler" << endl;
+	int length = sizeof(arrival) + 1;
+	for (int i = 0; i < length; i++)
+	{
+		cout << arrival[i] << "    " << burst[i] << "    " << priority[i]  << endl;
+	}
+}
