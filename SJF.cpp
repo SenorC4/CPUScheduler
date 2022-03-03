de8dd872f1113653b9e632004ffd4980c995dda1
LoginiStrong <83607947+LoginiStrong@users.noreply.github.com>
2022-03-02 17:27:12 -0800

    Finally got it all looking good and working. Basic changes like passing in the number of processes as an argument into each algorithm, as well as pretty printing for it all.

--- SJF.cpp
+++ SJF.cpp
@@ -1,13 +1,21 @@
-//This is the algorithm for the Shortest Job First
 #include <iostream>
 using namespace std;
 
-void sjfScheduler(int arrival[], int burst[], int priority[])
+<<<<<<< HEAD
+void sjfScheduler(int arrival[], int burst[], int priority[], int length)
 {
 	cout << "This is the shortest job first scheduler" << endl;
-	int length = sizeof(arrival);
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < length; i++)
 	{
-		cout << arrival[i] << "    " << burst[i] << "    " << priority[i]  << endl;
+		cout << "Arrival Time: " << arrival[i] << "    " << "Burst Time: " << burst[i] << "    " << "Priority: "  << priority[i]  << endl;
+=======
+void sjfScheduler(int* arrival, int* burst, int* priority)
+{
+	cout << "This is the shortest job first scheduler" << endl;
+	int length = sizeof(arrival)/sizeof(arrival[0]);
+	for (int i = 0; i < length; i++)
+	{
+		cout << arrival[i] << "	   " << burst[i] << "    " << priority[i] << endl;
+>>>>>>> 9aecdaeb32431dd1e0aba0b5237825ff5af868be
 	}
 }
--- main.cpp
+++ main.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
+<<<<<<< HEAD
 #include "SJF.h"
+#include "FCFS.h"
+=======
+>>>>>>> 9aecdaeb32431dd1e0aba0b5237825ff5af868be
 using namespace std;
-
 int main()
 {
 	int processNum;
-	cout << "Please enter the number of processes to be considered.";
+	cout << "Please enter the number of processes to be considered: ";
 	cin >> processNum;
 	int throughPutTime;
-	cout << "Please enter the throughput time for testing";
+	cout << "Please enter the throughput time for testing: ";
 	cin >> throughPutTime;
-	int arrivalTimes[3];
-	int burstTimes[3];
-	int priorities[3];
-	for(int i = 0; i < 3; i++)
+<<<<<<< HEAD
+	int arrivalTimes[processNum];
+	int burstTimes[processNum];
+	int priorities[processNum];
+=======
+	int* arrivalTimes = new int[10];
+	int* burstTimes = new int[10];
+	int* priorities = new int[10];
+>>>>>>> 9aecdaeb32431dd1e0aba0b5237825ff5af868be
+	for(int i = 0; i < processNum; i++)
 	{
+	cout << "Enter the processes arrival time followed by its burst time and the finally its priority: ";
 		int arrival,burst,priority;
 		cin >> arrival >> burst >> priority;
 		arrivalTimes[i] = arrival;
 		burstTimes[i] = burst;
 		priorities[i] = priority;
 	}
-	sjfScheduler(arrivalTimes, burstTimes, priorities);
+<<<<<<< HEAD
+	cout << endl;
+	sjfScheduler(arrivalTimes, burstTimes, priorities, processNum);
+	cout << endl;
+	fcfsScheduler(arrivalTimes, burstTimes, priorities, processNum);
+=======
+	return 0;
+>>>>>>> 9aecdaeb32431dd1e0aba0b5237825ff5af868be
 }
