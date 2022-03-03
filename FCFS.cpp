91a17c0935c44f1428333822264ad763116dc76c
Luke LeCain <lukelecain@gmail.com>
2022-03-03 13:03:05 -0800

    luke stuff

--- FCFS.cpp
+++ FCFS.cpp
@@ -6,8 +6,19 @@
 using namespace std;
 
 void fcfsScheduler(int arrival[], int burst[], int priority[], int length){
-	cout << "fcfs works" << endl;
-	for(int i = 0; i < length; i++){
-		cout << "Arrival time: " << arrival[i] << " Burst: " << burst[i] << " Priority: " << priority[i] << endl;
-	}
+	cout << "FCFS: \n";
+	//for(int i = 0; i < length; i++){
+	//	cout << "Arrival time: " << arrival[i] << " Burst: " << burst[i] << " Priority: " << priority[i] << endl;
+	//}
+
+	
+
+	//if arrival time > last process end time start time = arrival time
+	
+	cout << "Average wait time: ";
+	cout << "Shortest wait time: 0";
+	cout << "Longest wait time: ";
+	cout << "Average turn around time: ";
+	cout << "Throughput: " << length;
+	
 }
